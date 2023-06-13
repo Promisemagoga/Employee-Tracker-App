2dc8ace46abc3c7205a0646f1dc56024bdde0719
Cardw3ll <bonganicardwell@gmail.com>
2023-06-13 11:58:18 +0200

    13/06/2023

--- src/Components/DisplayEmployee.js
+++ src/Components/DisplayEmployee.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import UpdateEmployee from "./updateEmployees";
-// import SearchedResult from "./SearchedResult";
+import SearchedResult from "./SearchedResult";
 
 function DisplayEmployee(props) {
   let stringifyEmployee = localStorage.getItem("employeeProfiles");
@@ -20,6 +20,7 @@ function DisplayEmployee(props) {
   };
 
   return (
+    <div>
     <div>
       <table className="displayInfo">
         <tbody>
@@ -59,8 +60,8 @@ function DisplayEmployee(props) {
         </tbody>
       </table>
       {showForm && <UpdateEmployee />}
-      {/* <SearchedResult /> */}
-  
+  </div>
+     <SearchedResult />
     </div>
   );
 }
